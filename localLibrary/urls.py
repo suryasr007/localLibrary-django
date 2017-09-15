5c9e4d124d06faba928d848160f970f1b12e483e
Surya Teja Valluri <SV00483947@techmahindra.com>
2017-09-15 20:40:11 +0530

    basic Skeleton

--- localLibrary/urls.py
+++ localLibrary/urls.py
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.views.generic import RedirectView
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),    
 ]
+
+urlpatterns += [
+    url(r'^catalog/', include('catalog.urls')),
+]
+
+urlpatterns += [
+    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
+    #RedirectView.as_view() is used to map localhost:8000/catalog to localhost:8000
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
