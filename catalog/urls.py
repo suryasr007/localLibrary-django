028ae8a7cef0ef8c89ab8bd17a1739fcc8e0ba56
Surya Teja Valluri <SV00483947@techmahindra.com>
2017-09-23 18:33:03 +0530

    part 6

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
-    url(r'^books/$',views.BookListView.as_view(),name='books'),    
+    url(r'^books/$',views.BookListView.as_view(),name='books'),  
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),  
 ]
