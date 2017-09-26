9530e772d6c0ee83cc83655b4491e2fd145de119
Surya Teja Valluri <SV00483947@techmahindra.com>
2017-09-26 21:25:52 +0530

    part 6 Full

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^books/$',views.BookListView.as_view(),name='books'),  
     url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),  
+    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),   
+    url(r'^authors/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),     
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -25,6 +25,14 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 10
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
